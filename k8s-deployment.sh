867b738e45be7b00a495fc31cc2a972e05528f8f
yulian6766 <yulian6766@gmail.com>
2022-06-24 20:26:13 -0500

    Added rollout stage K8s

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -2,7 +2,7 @@
 
 #k8s-deployment.sh
 
-sed -i "s#replace#${imageName}#g" k8s_deployment_service.yaml
+sed -i "s#replace#${IMAGETAG}#g" k8s_deployment_service.yaml
 kubectl -n default get deployment ${deploymentName} > /dev/null
 
 if [[ $? -ne 0 ]]; then
@@ -10,6 +10,6 @@ if [[ $? -ne 0 ]]; then
     kubectl -n default apply -f k8s_deployment_service.yaml
 else
     echo "deployment ${deploymentName} exist"
-    echo "image name - ${imageName}"
-    kubectl -n default set image deploy ${deploymentName} ${containerName}=${imageName} --record=true
+    echo "image name - ${IMAGETAG}"
+    kubectl -n default set image deploy ${deploymentName} ${containerName}=${IMAGETAG} --record=true
 fi
